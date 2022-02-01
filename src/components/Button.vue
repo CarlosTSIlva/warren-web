041348bf9f75e809628af2ee34c2a4e78b57372d
carlosWarren <carlos.silva@warren.com.br>
2022-02-01 13:59:15 -0300

    first comiit

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button :style="{backgroundColor: color}" @click="onClick">{{ texto }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    texto: String,
+    color: String,
+    onClick: Function
+  }
+}
+</script>
+
+<style scoped>
+button {
+    background: #EE2E5D;
+}
+</style>
